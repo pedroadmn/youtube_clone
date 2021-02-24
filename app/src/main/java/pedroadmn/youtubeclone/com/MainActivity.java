383c0e888735956d97ccf8a7067753f96f1ae8d2
Pedro Adelino de Mello Neto <pedro.mello@virtus.ufcg.edu.br>
2021-02-24 11:20:11 -0300

    adding video control events

--- app/src/main/java/pedroadmn/youtubeclone/com/MainActivity.java
+++ app/src/main/java/pedroadmn/youtubeclone/com/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
     private YouTubePlayerView youTubePlayerView;
     private static final String GOOGLE_API_KEY = "AIzaSyC71w5ruIWujm7_9nKsbwUDirFHOtBWhT0";
 
+    private YouTubePlayer.PlaybackEventListener playbackEventListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +25,44 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
         youTubePlayerView = findViewById(R.id.viewYoutubePlayer);
 
         youTubePlayerView.initialize(GOOGLE_API_KEY, this);
+
+        playbackEventListener = new YouTubePlayer.PlaybackEventListener() {
+            @Override
+            public void onPlaying() {
+                Toast.makeText(MainActivity.this, "Executing Video", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onPaused() {
+                Toast.makeText(MainActivity.this, "Paused Video", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStopped() {
+                Toast.makeText(MainActivity.this, "Stopped Video", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onBuffering(boolean b) {
+                Toast.makeText(MainActivity.this, "Buffering Video", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onSeekTo(int i) {
+                Toast.makeText(MainActivity.this, "Seek Video", Toast.LENGTH_LONG).show();
+            }
+        };
     }
 
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
 //        youTubePlayer.loadVideo("byQtPUOXzu4");
 
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
+
         if (!wasRestored) {
-            youTubePlayer.cueVideo("byQtPUOXzu4");
+//            youTubePlayer.cueVideo("byQtPUOXzu4");
+            youTubePlayer.cuePlaylist("PLWz5rJ2EKKc98e0f5ZbsgB63MdjZTFgsy");
         }
 
         Toast.makeText(this, "Initialized Success", Toast.LENGTH_LONG).show();
@@ -38,6 +70,6 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
 
     @Override
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-        Toast.makeText(this, "Initialized Failure", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Initialized Failure: " + youTubeInitializationResult.toString(), Toast.LENGTH_LONG).show();
     }
 }
